64a281a440a71930b5a3f1970abd27344a397ffd
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-20 12:16:47 -0700

    filled out functions from lecture

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -7,21 +7,47 @@ class ArrayList
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    raise "bad things" if @size == 0
+    @size -= 1
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
+    # count = 0
+    # @size.times do
+    #   count += 1
+    # end
+    # return count
   end
 
   def max
+    biggest = @storage[0]
+    @size.times do |i|
+      if @storage[i] > biggest
+        biggest = storage[i]
+      end
+    end
+    return biggest
   end
 
   def to_s
+    str = ""
+    @size.length.times do |i|
+      str += @storage[i] + ", "
+    end
+    return "#{str[0..-3]}"
   end
 end
